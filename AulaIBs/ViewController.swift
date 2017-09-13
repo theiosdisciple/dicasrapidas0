bdc96990e85b21181c6783903dd99d1e2bcc12f5
Gabriel Bezerra Valério <gbrvalerio@gmail.com>
2017-09-13 16:40:39 -0300

    Initial Commit

---
+++ AulaIBs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AulaIBs
+//
+//  Created by Gabriel Bezerra Valério on 13/09/17.
+//  Copyright © 2017 theiosdisciple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
